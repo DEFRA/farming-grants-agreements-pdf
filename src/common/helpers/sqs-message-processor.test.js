1d4ca090b22f507979b7074dc49bdfce21fa8c3b
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2026-02-10 15:42:23 +0000

    SFIR-990: Fix date typos

--- src/common/helpers/sqs-message-processor.test.js
+++ src/common/helpers/sqs-message-processor.test.js
@@ -374,7 +374,7 @@ describe('SQS message processor', () => {
           version: 1,
           status: 'accepted',
           agreementUrl: 'https://example.com/agreement/FPTT123456789',
-          agreementEndData: '2027-01-01'
+          agreementEndDate: '2027-01-01'
         }
       }
 
@@ -489,7 +489,7 @@ describe('SQS message processor', () => {
           version: 2,
           status: 'accepted',
           agreementUrl: 'https://example.com/agreement/FPTT123456789',
-          agreementEndData: '2027-12-31'
+          agreementEndDate: '2027-12-31'
         }
       }
 
@@ -549,7 +549,7 @@ describe('SQS message processor', () => {
           version: 1,
           status: 'accepted',
           agreementUrl: 'https://example.com/agreement/FPTT123456789',
-          agreementEndData: '2027-01-01'
+          agreementEndDate: '2027-01-01'
         }
       }
 
