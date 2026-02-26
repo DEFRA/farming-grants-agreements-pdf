73248c5fd52381e59c0b0104da8f89ef36ff6d6b
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2026-02-26 16:13:00 +0000

    SFIR-1021 Bump packages & add version

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,7 +1,13 @@
+import { config } from '#~/config.js'
+
 const health = {
   method: 'GET',
   path: '/health',
-  handler: (_request, h) => h.response({ message: 'success' })
+  handler: (_request, h) =>
+    h.response({
+      message: 'success',
+      version: config.get('serviceVersion') || 'dev'
+    })
 }
 
 export { health }
