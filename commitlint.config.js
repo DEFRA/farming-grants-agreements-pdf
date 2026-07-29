c52323e7d6c12b702b76357e4b09acb83a540166
Justin Ramel <justin.ramel@gmail.com>
2026-07-29 21:03:53 +0100

    no-mistakes: apply CI fixes

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,8 +6,10 @@ export default {
     {
       rules: {
         'ticket-format': ({ header }) => [
-          /^[A-Z]+-\d+(?::\s*)?\s+.+$/.test(header),
-          'Commit message must be in the format: TICKET-123: message'
+          /^(?:[A-Z]+-\d+(?::\s*)?\s+.+|[a-z][a-z0-9-]*(?:\([^)]+\))?!?:\s+.+)$/.test(
+            header
+          ),
+          'Commit message must use either "TICKET-123: message" or "type(scope): message"'
         ]
       }
     }
